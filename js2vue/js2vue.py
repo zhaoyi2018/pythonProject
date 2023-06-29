5e4e149cadad684b81f6fca06daf6529bc3efef4
zhaoyi <zhaoyi@163.com>
2023-06-29 10:59:58 +0800

    zhaoyi： 更新

--- js2vue/js2vue.py
+++ js2vue/js2vue.py
@@ -35,8 +35,8 @@ def match_content_to_index(content, start_index):
         "{": "}",
         "[": "]",
         "(": ")",
-        "\'": "\'",
-        "\"": "\""
+        '"': '"',
+        "'": "'"
     }
     char_queue.append(char_dict.get(content[start_index]))
     char_num = 1
@@ -47,15 +47,26 @@ def match_content_to_index(content, start_index):
         if content[i] in ["}", ")", "]"] and char_queue[-1] == content[i]:
             char_queue.pop()
             char_num -= 1
-        if content[i] == "\'":
-            if char_queue[-1] == "\'":
+        elif content[i] in ["}", ")", "]"] and char_queue[-2] == content[i] and char_queue[-1] in ["'", '"']:
+            char_queue.pop()
+            char_num -= 1
+            char_queue.pop()
+            char_num -= 1
+        elif content[i] in ["}", ")", "]"] and content[i] in char_queue:
+            temp = -1
+            while content[i] != char_queue[temp]:
+                temp -= 1
+            char_num += temp
+            char_queue = char_queue[:temp]
+        if content[i] == "'":
+            if char_queue[-1] == "'":
                 char_queue.pop()
                 char_num -= 1
             else:
                 char_queue.append(char_dict.get(content[i]))
                 char_num += 1
-        if content[i] == "\"":
-            if char_queue[-1] == "\"":
+        if content[i] == '"':
+            if char_queue[-1] == '"':
                 char_queue.pop()
                 char_num -= 1
             else:
@@ -185,15 +196,16 @@ def get_attr_dict(attr_str, inner_elements):
             attr_dict["slot-scope"] = '"scope"'
             # 子级元素补充，记住我们只需要获取子集元素的字典就行
             # 1. 首先先剥壳
-            temp_inner_elements = to_husk(attr_value, len("_vm._u([{key:\"default\",fn:function(scope){return ["))
+            stop_index = attr_value.find("return ")
+            temp_inner_elements = to_husk(attr_value, stop_index+7)
             # 2. 开始循环处理
             while len(temp_inner_elements.strip(" ,")) > 0:
                 if temp_inner_elements[:2] == "_c":
                     temp_end = match_content_to_index(temp_inner_elements, 2)
-                    temp_inner_dict = split_inner_from_content(temp_inner_elements[:temp_end+1])
+                    temp_inner_dict = split_inner_from_content(temp_inner_elements[:temp_end + 1])
                     inner_elements.append(temp_inner_dict)
-                    if temp_end+1 < len(temp_inner_elements):
-                        temp_inner_elements = temp_inner_elements[temp_end+1:].strip(" ,")
+                    if temp_end + 1 < len(temp_inner_elements):
+                        temp_inner_elements = temp_inner_elements[temp_end + 1:].strip(" ,")
                     else:
                         break
                 elif temp_inner_elements[0] == "(":
@@ -206,13 +218,16 @@ def get_attr_dict(attr_str, inner_elements):
                     second_content, temp_inner_elements = match_content(second_content)
                     # 子级元素内容生成后，存储的字典
                     fisrt_content = split_inner_from_content(fisrt_content)
-                    fisrt_content['element_attr'] = "{v-if:" + "\"" + condition + "\"" + ", " + fisrt_content['element_attr'][1:]
+                    fisrt_content['element_attr'] = "{v-if:" + "\"" + condition + "\"" + ", " + fisrt_content[
+                                                                                                    'element_attr'][1:]
                     inner_elements.append(fisrt_content)
                     second_content = split_inner_from_content(second_content)
                     second_content['element_attr'] = "{v-else:'', " + second_content['element_attr'][1:]
                     inner_elements.append(second_content)
                 elif temp_inner_elements[:11] == '_vm._v(" ")':
                     temp_inner_elements = temp_inner_elements[11:].strip(" ,")
+                elif temp_inner_elements[0] == '[':
+                    temp_inner_elements = temp_inner_elements[1: -1]
                 else:
                     print("这是scopedSlots判断, 有问题:", temp_inner_elements)
                     break
@@ -223,7 +238,7 @@ def get_attr_dict(attr_str, inner_elements):
                     attr_dict["@" + key] = "\"" + value[4:] + "\""
                 elif key[0] == ":":
                     attr_dict["@" + key[1:]] = value
-        elif attr_value[0] not in ['"', "'", "{", "[", "@"]:
+        elif attr_value != '' and attr_value[0] not in ['"', "'", "{", "[", "@"]:
             # 以_vm开头
             if attr_value.find("_vm.") >= 0:
                 attr_dict[":" + attr_key] = "\"" + attr_value.replace("_vm.", "") + "\""
@@ -235,8 +250,12 @@ def get_attr_dict(attr_str, inner_elements):
                 1) + "\""
         elif attr_key == "directives" and attr_value[0] == "[":
             temp_key = re.search("rawName:\"([-a-zA-Z0-9_]*)\"", attr_value).group(1)
-            temp_value = re.search("expression:\"([-a-zA-Z0-9_\\[\\]:'\\.]*)\"", attr_value).group(1)
-            attr_dict[temp_key] = "\"" + temp_value + "\""
+            if "expression" not in attr_value:
+                print("特殊情况:", attr_value)
+                attr_dict[temp_key] = ""
+            else:
+                temp_value = re.search("expression:\"([-a-zA-Z0-9_\\[\\]$:'\\.!=&| ]*)\"", attr_value).group(1)
+                attr_dict[temp_key] = "\"" + temp_value + "\""
         else:
             attr_dict[attr_key] = attr_value
         if attr_value_end_index + 2 < len(attr_str):
@@ -246,7 +265,6 @@ def get_attr_dict(attr_str, inner_elements):
     return attr_dict
 
 
-
 def to_husk(string, stop_index):
     """
     剥壳
@@ -254,7 +272,7 @@ def to_husk(string, stop_index):
     :param stop_index: 最终停止位置
     :return:
     """
-    if stop_index < 0 or string[stop_index - 1] not in ['"', "'", "[", "(", "{", "]", ")", "}"]:
+    if stop_index < 0 or (string[stop_index] not in ['"', "'", "[", "(", "{", "]", ")", "}"] and string[stop_index:stop_index+2] != "_c"):
         print("输入参数有问题：", string, string[stop_index - 1], stop_index)
         return string
     start = 0
@@ -286,7 +304,8 @@ def match_content(string):
             start += 1
         else:
             end_index = match_content_to_index(string, start)
-            return string[:end_index+1].strip(" ,[]"), string[end_index+1:].strip(" ,:[]")
+            return string[:end_index + 1].strip(" ,[]"), string[end_index + 1:].strip(" ,:[]")
+
 
 def dict_to_vue(data_dict, nums=0):
     element_str = ""
@@ -310,7 +329,7 @@ def dict_to_vue(data_dict, nums=0):
             if inner_content != "":
                 one_element_str += "\n" + ("\t" * (nums + 1)) + inner_content + "\n" + ("\t" * nums)
         one_element_str += "</" + data_dict["element_tag"] + ">"
-        one_element_str *= data_dict.get("element_nums", 1)
+        # one_element_str *= data_dict.get("element_nums", 1)
         element_str = one_element_str
     elif data_dict["element_type"] == "_vm._v":
         if data_dict["element_value"].find("_vm._s") >= 0:
@@ -329,5 +348,44 @@ def dict_to_vue(data_dict, nums=0):
 
 
 file_content = read_file_content("/data/home/zhaoyi/Documents/PycharmProjects/pythonProject/js2vue/js.txt")
+# 处理换行
+file_content = file_content.replace("\n", "").replace("\t", " ").replace("\\n", "")
+# 数据处理前, 先将三元表达式(? :), v-for循环(vm._l)替换为可处理文本
+# 1.先处理三元表达式
+while file_content.find(")?") > 0:
+    index1 = file_content.find(")?") + 1
+    index0 = file_content[:index1].rfind("(")
+    # ?后面有两种情况，第一种_c(...), 第二种[...]
+    if file_content[index1 + 1:index1 + 3] == "_c":
+        index2 = match_content_to_index(file_content, index1 + 3) + 1
+    elif file_content[index1 + 1] == "[":
+        index2 = match_content_to_index(file_content, index1 + 1) + 1
+    else:
+        print("?后面有:", index1, file_content[index1 + 1], file_content[index1 - 3:index1 + 3])
+        break
+    print("下标：", index0, index1, index2)
+    print("头部条件：", file_content[index0: index1 + 1])
+    print("冒号查看：", file_content[index2-1: index2 + 10])
+    second_content = match_content(file_content[index2+1:])[0]
+    print("第一个匹配内容:", second_content, len(second_content), file_content[index2+1+len(second_content):index2+1+len(second_content)+5])
+    file_content = file_content[:index2] + "," + file_content[index2 + 1:]
+    file_content = file_content[:index0] + file_content[index1 + 1:]
+# 2.处理v-for
+while file_content.find("_vm._l") > 0:
+    index0 = file_content.find("_vm._l")
+    index1 = file_content[index0:].find("return ") + 7
+    index4 = match_content_to_index(file_content, index0 + 6)
+    index3 = match_content_to_index(file_content, index1 + index0 + 2)
+    print(index0, index1, index3, index4)
+    print("头部:", file_content[index0: index1 + index0])
+    print("尾部:", file_content[index3: index4+2])
+    print("全部:", file_content[index0: index4+2])
+    key = re.search("_vm\\._l\\(\\(([_a-zA-Z.\\[\\]]+?)\\)", file_content[index0: index1 + index0]).group(1).replace("_vm.", "")
+    value = re.search("function([()a-z A-Z,]*?)\\{", file_content[index0: index1 + index0]).group(1)
+    print("Key:", key, " Value:", value, "_c('for', {value:'" + value + " in " + key + "'}, [")
+    file_content = file_content[:index3 + 1] + "])" + file_content[index4 + 1:]
+    file_content = file_content[:index0] + "_c('for', {value:'" + value + " in " + key + "'}, [" + \
+                   file_content[index1 + index0:]
+
 print(dict_to_vue(split_inner_from_content(file_content)))
 # print(to_husk(file_content, len("([{key:\"default\",fn:function(scope){return [")))
